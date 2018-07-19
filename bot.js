63278ef89ebb19b85059710962fe624cbab8388f
OMARbelabdoune <41428371+OMARbelabdoune@users.noreply.github.com>
2018-07-19 13:32:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.help'
+const prefix = '*help'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
