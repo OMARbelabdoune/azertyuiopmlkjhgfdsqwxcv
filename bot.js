de67ab09794e744d0ad8e23ca2c34defb765762d
OMARbelabdoune <41428371+OMARbelabdoune@users.noreply.github.com>
2018-07-19 12:55:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,11 +27,6 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message' , message => {
-    if(message.content.includes === 'الكلام') {
-    message.channel.send(`الرد`);
-    }
-     });
 
 
 
