6c864fd64356123f6d9cd8371dde55b2417879ca
OMARbelabdoune <41428371+OMARbelabdoune@users.noreply.github.com>
2018-07-19 15:20:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = " * " ;
+const prefix = "." ;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -26,10 +26,9 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-});
-client.on('message', message => {
+});client.on('message', message => {
     if (message.author.bot) return;
-     if (message.content === prefix + "*help") {
+     if (message.content === prefix + "help-en") {
 		 message.channel.send('**The Message Was Sent On Private**');
             
 	
@@ -96,13 +95,7 @@ client.on('message', message => {
 
 ❖ *invite ➾ bot invite link
 
-╔[❖════════════❖]╗
-                    Welcome
-╚[❖════════════❖]╝
 
-to enable welcome message do channel name "wlc"
-
-=
 
 `);
 
